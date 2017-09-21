6166c95a1abb5cfa1cbe98590b78b15961cf6597
YMlion <lm_yao@163.com>
2017-09-21 16:30:50 +0800

    aux_source_directory; delete unused files

--- app/src/main/jni/yuvtool.c
+++ app/src/main/jni/yuvtool.c
@@ -17,18 +17,14 @@
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
 
-#define FOURCC(a, b, c, d)                                 \
-  (((uint32_t)(a)) | ((uint32_t)(b) << 8) |       /* NOLINT */ \
-   ((uint32_t)(c) << 16) | ((uint32_t)(d) << 24)) /* NOLINT */
-
 JNIEXPORT void JNICALL
 Java_com_ymlion_mediasample_util_YuvUtil_convertToRgba(JNIEnv *env, jclass type,
                                                        jbyteArray yuvBytes_, jint width,
                                                        jint height, jbyteArray rgba_, jint mode) {
 
     LOGD("convert to argb");
-    uint8_t *yuvData = (uint8_t *) (*env)->GetByteArrayElements(env, yuvBytes_, NULL);
-    uint8_t *rgbData = (uint8_t *) (*env)->GetByteArrayElements(env, rgba_, NULL);
+    uint8 *yuvData = (uint8 *) (*env)->GetByteArrayElements(env, yuvBytes_, NULL);
+    uint8 *rgbData = (uint8 *) (*env)->GetByteArrayElements(env, rgba_, NULL);
 
     int rgba_stride = width * 4;
     int y_stride = width;
@@ -46,7 +42,7 @@ Java_com_ymlion_mediasample_util_YuvUtil_convertToRgba(JNIEnv *env, jclass type,
             break;
         default:
             ConvertToARGB(yuvData, ySize * 3 / 2, rgbData, rgba_stride, 0, 0, width, height, width,
-                          height, 0, FOURCC('N', 'V', '1', '2'));
+                          height, (enum RotationMode) 0, FOURCC('N', 'V', '1', '2'));
     }
 
     (*env)->ReleaseByteArrayElements(env, yuvBytes_, (jbyte *) yuvData, 0);
@@ -56,7 +52,7 @@ Java_com_ymlion_mediasample_util_YuvUtil_convertToRgba(JNIEnv *env, jclass type,
 JNIEXPORT void JNICALL
 Java_com_ymlion_mediasample_util_YuvUtil_scaleNV21(JNIEnv *env, jclass type, jbyteArray src,
                                                    jint width, jint height, jbyteArray dst,
-                                                   jint dst_width, jint dst_height, int mode) {
+                                                   jint dst_width, jint dst_height, jint mode) {
     jbyte *srcData = (*env)->GetByteArrayElements(env, src, NULL);
     jbyte *dstData = (*env)->GetByteArrayElements(env, dst, NULL);
 
@@ -70,11 +66,11 @@ Java_com_ymlion_mediasample_util_YuvUtil_scaleNV21(JNIEnv *env, jclass type, jby
     int dst_v_stride = dst_u_stride;
     size_t dst_ySize = (size_t) (dst_y_stride * dst_height);
     size_t dst_uSize = (size_t) (dst_ySize >> 2);
-    I420Scale((uint8_t *) srcData, width, (uint8_t *) srcData + ySize, u_stride,
-              (uint8_t *) srcData + ySize + uSize, v_stride, width, height, (uint8_t *) dstData,
-              dst_y_stride, (uint8_t *) dstData + dst_ySize, dst_u_stride,
-              (uint8_t *) dstData + dst_ySize + dst_uSize, dst_v_stride, dst_width, dst_height,
-              mode);
+    I420Scale((uint8 *) srcData, width, (uint8 *) srcData + ySize, u_stride,
+              (uint8 *) srcData + ySize + uSize, v_stride, width, height, (uint8 *) dstData,
+              dst_y_stride, (uint8 *) dstData + dst_ySize, dst_u_stride,
+              (uint8 *) dstData + dst_ySize + dst_uSize, dst_v_stride, dst_width, dst_height,
+              (enum FilterMode) mode);
 
     (*env)->ReleaseByteArrayElements(env, src, srcData, 0);
     (*env)->ReleaseByteArrayElements(env, dst, dstData, 0);
@@ -91,8 +87,8 @@ Java_com_ymlion_mediasample_util_YuvUtil_scaleARGB(JNIEnv *env, jclass type, jby
     int dst_stride = 4 * dstWidth;
     // TODO
 
-    ARGBScale((uint8_t *) src, src_stride, width, height, (uint8_t *) dst, dst_stride, dstWidth,
-              dstHeight, mode);
+    ARGBScale((uint8 *) src, src_stride, width, height, (uint8 *) dst, dst_stride, dstWidth,
+              dstHeight, (enum FilterMode) mode);
 
     (*env)->ReleaseByteArrayElements(env, src_, src, 0);
     (*env)->ReleaseByteArrayElements(env, dst_, dst, 0);
@@ -112,7 +108,8 @@ Java_com_ymlion_mediasample_util_YuvUtil_rotateARGB(JNIEnv *env, jclass type, jb
         dst_stride = width * 4;
     }
 
-    ARGBRotate((const uint8_t *) src, src_stride, (uint8_t *) dst, dst_stride, width, height, mode);
+    ARGBRotate((const uint8 *) src, src_stride, (uint8 *) dst, dst_stride, width, height,
+               (enum RotationMode) mode);
 
     (*env)->ReleaseByteArrayElements(env, src_, src, 0);
     (*env)->ReleaseByteArrayElements(env, dst_, dst, 0);
@@ -135,15 +132,15 @@ Java_com_ymlion_mediasample_util_YuvUtil_convertToARGB(JNIEnv *env, jclass type,
     switch (format) {
         case 1:
             NV12ToARGB(yuvData, y_stride, yuvData + ySize, (width + 1) / 2 * 2,
-                       rgbData, rgba_stride, width, height);
+                       (uint8 *) rgbData, rgba_stride, width, height);
             break;
         case 2:
             NV21ToARGB(yuvData, y_stride, yuvData + ySize, (width + 1) / 2 * 2,
-                       rgbData, rgba_stride, width, height);
+                       (uint8 *) rgbData, rgba_stride, width, height);
             break;
         default:
-            ConvertToARGB(yuvData, ySize * 3 / 2, rgbData, rgba_stride, 0, 0, width, height, width,
-                          height, 0, FOURCC('N', 'V', '1', '2'));
+            ConvertToARGB(yuvData, ySize * 3 / 2, (uint8 *) rgbData, rgba_stride, 0, 0, width, height, width,
+                          height, (enum RotationMode) 0, FOURCC('N', 'V', '1', '2'));
     }
 
     char *rotateData;
@@ -157,8 +154,8 @@ Java_com_ymlion_mediasample_util_YuvUtil_convertToARGB(JNIEnv *env, jclass type,
             dst_stride = width * 4;
         }
 
-        ARGBRotate((const uint8_t *) rgbData, src_stride, (uint8_t *) rotateData, dst_stride, width,
-                   height, orientation);
+        ARGBRotate((const uint8 *) rgbData, src_stride, (uint8 *) rotateData, dst_stride, width,
+                   height, (enum RotationMode) orientation);
     } else {
         rotateData = rgbData;
     }
@@ -190,9 +187,9 @@ Java_com_ymlion_mediasample_util_YuvUtil_convertToARGB(JNIEnv *env, jclass type,
     if (ANativeWindow_lock(nativeWindow, &windowBuffer, NULL) < 0) {
         LOGD("cannot lock window");
     } else {
-        uint8_t *dst = (uint8_t *) windowBuffer.bits;
-        ARGBScale((uint8_t *) rotateData, src_stride, w, h, dst, windowBuffer.stride * 4, dstWidth,
-                  dstHeight, scaleMode);
+        uint8_t *dst = (uint8 *) windowBuffer.bits;
+        ARGBScale((uint8 *) rotateData, src_stride, w, h, dst, windowBuffer.stride * 4, dstWidth,
+                  dstHeight, (enum FilterMode) scaleMode);
         ANativeWindow_unlockAndPost(nativeWindow);
     }
 
